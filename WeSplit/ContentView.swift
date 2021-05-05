78b9b1a4b6f711e78b822542c0c4845547d0ed6e
Esben Viskum <viskum@viskum.dk>
2021-05-05 13:27:03 +0200

    Finished WeSplit app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var noPeople = ""
+    @State private var tipAmount = 2
+    let tips = [10, 15, 20, 25, 0]
+    
+    var grandTotal: Double {
+        let tipSelection = Double(tips[tipAmount])
+        let orderAmount = Double(checkAmount) ?? 0
+
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double? {
+        let peopleCount = Double(noPeople) ?? 0
+        if peopleCount > 0 {
+            let amountPerPerson = grandTotal / peopleCount
+            return amountPerPerson
+        }
+        return nil
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Beløb", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    TextField("Antal personer", text: $noPeople)
+                        .keyboardType(.decimalPad)
+                    
+                }
+                
+                Section(header: Text("Hvor meget i drikkepenge?")) {
+                    Picker("Drikkepenge", selection: $tipAmount) {
+                        ForEach(0..<tips.count) { i in
+                            Text("\(tips[i])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Total beløb")) {
+                    Text("kr. \(grandTotal, specifier: "%.2f")")
+                        .foregroundColor(tipAmount == 4 ? .red : .primary
+                        )
+                }
+                
+                Section(header: Text("Beløb per person")) {
+                    if let totalPerPerson = totalPerPerson {
+                        Text("kr. \(totalPerPerson, specifier: "%.2f")")
+                    } else {
+                        Text("kr.")
+                    }
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
